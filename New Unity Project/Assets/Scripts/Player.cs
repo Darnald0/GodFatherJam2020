04472c3de9d48a166da491c07e21c86d3d414fed
f.jouanneau <f.jouanneau@student.isartdigital.com>
2020-09-02 14:10:48 +0200

    Adding tree, can cut and take wood

--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private int wood = 0;
+    public int wood = 0;
 
     // Start is called before the first frame update
     void Start()
--- New Unity Project/Assets/Scripts/Tree.cs
+++ New Unity Project/Assets/Scripts/Tree.cs
@@ -4,15 +4,97 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int health = 2;
+    private float realTimeToDamage = 2f;
+    private bool isIn = false;
+    private Player player = null;
+
+    private float timeToGoUp = 0f;
+    private float distToGoUp = 0f;
+
+    [SerializeField] private float timeToDamage = 2f;
+    [SerializeField] private int woodsGiven = 2;
+    [SerializeField] private GameObject spriteBarTree = null;
+    [SerializeField] private GameObject spriteBarWhite = null;
+    
+
+    private void Start()
     {
-        
+        realTimeToDamage = timeToDamage;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isIn)
+        {
+            if (Input.GetKey(KeyCode.E) && realTimeToDamage > 0f && health > 1)
+            {
+                realTimeToDamage -= Time.deltaTime;
+                spriteBarTree.transform.localPosition = new Vector3(spriteBarTree.transform.localPosition.x, spriteBarTree.transform.localPosition.y - (spriteBarTree.transform.localScale.y / timeToDamage) * Time.deltaTime, spriteBarTree.transform.localPosition.z);
+            }
+            else if (realTimeToDamage <= 0f)
+            {
+                realTimeToDamage = timeToDamage;
+                health--;
+                player.wood += woodsGiven;
+                transform.position = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+                spriteBarTree.transform.localPosition = new Vector3(spriteBarTree.transform.localPosition.x, 0.3f, spriteBarTree.transform.localPosition.z);
+            }
+        }
+        else
+        {
+            if (realTimeToDamage < timeToDamage && health > 1)
+            {
+                realTimeToDamage += Time.deltaTime;
+                spriteBarTree.transform.localPosition = new Vector3(spriteBarTree.transform.localPosition.x, spriteBarTree.transform.localPosition.y - ( distToGoUp / timeToGoUp) * Time.deltaTime, spriteBarTree.transform.localPosition.z);
+            }
+            else if (realTimeToDamage > timeToDamage)
+            {
+                realTimeToDamage = timeToDamage;
+                spriteBarTree.transform.localPosition = new Vector3(spriteBarTree.transform.localPosition.x, 0.3f, spriteBarTree.transform.localPosition.z);
+            }
+        }
+
+        if (health == 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void EnnemyAttack()
+    {
+
+    }
+
+    public void PassDay()
+    {
+        if (health == 1)
+        {
+            health++;
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+            return;
+
+        if (player == null)
+            player = collision.gameObject.GetComponent<Player>();
+
+        isIn = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+            return;
+
+        isIn = false;
+        timeToGoUp = timeToDamage - realTimeToDamage;
+        distToGoUp = spriteBarTree.transform.localPosition.y - 0.3f;
+        //realTimeToDamage = timeToDamage;
+        //spriteBarTree.transform.localPosition = new Vector3(spriteBarTree.transform.localPosition.x, 0.3f, spriteBarTree.transform.localPosition.z);
     }
 }
